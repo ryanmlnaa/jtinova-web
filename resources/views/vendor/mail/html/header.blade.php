6f1bd0aa9fba3a767101ac6078279f978869a283
bangik <aldi.novaldi.frmnsyah@gmail.com>
2024-06-25 12:58:28 +0700

    fix: logo di email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'TEFA JTI Innovation')
-<img src="{{asset('static/logo.png')}}" class="logo" alt="TEFA JTI Innovation">
+<img src="{{asset('static/logo-full.png')}}" class="logo" alt="TEFA JTI Innovation">
 @else
 {{ $slot }}
 @endif
