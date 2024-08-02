8f5948c082e995c889fa8cbd819e8092edd6e26b
bangik <aldi.novaldi.frmnsyah@gmail.com>
2024-08-02 11:02:42 +0700

    add: silabus to timeline

--- resources/views/admin/timeline/edit.blade.php
+++ resources/views/admin/timeline/edit.blade.php
@@ -2,141 +2,184 @@
 @section('title', $title)
 @section('menu', $title)
 @section('content')
-<div class="section-body">
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="card">
-                <div class="card-header">
-                    <h4>Tambah Data</h4>
-                </div>
-                <div class="card-body">
-                  <form action="{{route('timeline.update', $timeline)}}" method="post" enctype="multipart/form-data">
-                    @csrf
-                    @method('PUT')
-                    <div class="form-group">
-                      <label for="title" class="col-form-label">Judul</label> <span class="text-danger">*</span>
-                      <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" id="title" value="{{old('title', $timeline->title)}}" required>
-                        @error('title')
-                        <div class="invalid-feedback">
-                            {{ $message }}
-                        </div>
-                        @enderror
+    <div class="section-body">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Tambah Data</h4>
                     </div>
-                    <div class="form-group">
-                      <label for="title" class="col-form-label">Judul</label> <span class="text-danger">*</span>
-                      <select class="form-control @error('jenis') is-invalid @enderror" name="jenis" id="jenis" required>
-                        <option value="">Pilih Jenis</option>
-                        <option value="mbkm" {{old('jenis', $timeline->jenis) == 'mbkm' ? 'selected' : ''}}>MBKM</option>
-                        <option value="instruktur" {{old('jenis', $timeline->jenis) == 'instruktur' ? 'selected' : ''}}>Instruktur</option>
-                        <option value="freelance" {{old('jenis', $timeline->jenis) == 'freelance' ? 'selected' : ''}}>Freelance</option>
-                      </select>
-                        @error('jenis')
-                        <div class="invalid-feedback">
-                            {{ $message }}
-                        </div>
-                        @enderror
-                    </div>
-                    <div class="form-group">
-                      <label for="deskripsi" class="col-form-label">Deskripsi</label> <span class="text-danger">*</span>
-                      <textarea class="summernote-simple @error('deskripsi') is-invalid @enderror" name="deskripsi" id="deskripsi" required>{{old('tahun_ajaran', $timeline->tahun_ajaran)}}</textarea>
-                        @error('deskripsi')
-                        <div class="invalid-feedback">
-                            {{ $message }}
-                        </div>
-                        @enderror
-                    </div>
-                    <div class="form-group">
-                      <label for="status" class="col-form-label">Status</label> <span class="text-danger">*</span>
-                      <select name="status" id="status" class="form-control @error('status') is-invalid @enderror" required>
-                        <option value="">Pilih Status</option>
-                        <option value="1" {{old('status', $timeline->status) == 1 ? 'selected' : ''}}>Aktif</option>
-                        <option value="0" {{old('status', $timeline->status) == 0 ? 'selected' : ''}}>Tidak Aktif</option>
-                      </select>
-                        @error('status')
-                        <div class="invalid-feedback">
-                            {{ $message }}
-                        </div>
-                        @enderror
-                    </div>
-                    <div class="row form-group">
-                      <label for="description" class="col-xxl-3 col-xl-3 col-lg-3 col-md-3 col-form-label text-left pl-3">Deskripsi <span class="text-danger">*</span></label> 
-                      <label for="start_at" class="col-xxl-3 col-xl-3 col-lg-3 col-md-3 col-form-label text-left pl-3">Tanggal Buka <span class="text-danger">*</span></label> 
-                      <label for="end_at" class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-form-label text-left pl-6">Tanggal Tutup <span class="text-danger">*</span></label> 
-                    </div>
-                    <div class="row form-group input-copy-here">
-                      @foreach (json_decode($timeline->timeline) as $item)
-                      @if(!$loop->first)
-                      <div class="input-group">
-                      @endif
-                      <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-3">
-                        <div class="form-group">
-                          <input type="text" class="form-control" name="description[]" id="description" value="{{$item->title}}" required>
-                        </div>
-                      </div>
-                      <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-3">
-                        <input type="date" class="form-control" name="start_at[]" id="start_at" value="{{$item->start}}" required>
-                      </div>
-                      <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6">
-                        <div class="row">
-                          <div class="col-xxl-11 col-xl-11 col-lg-11 col-md-11 col-sm-11">
-                            <input type="date" class="form-control" name="end_at[]" id="end_at" value="{{$item->end}}" required>
-                          </div>
-                          <div class="col-xxl-1 col-xl-1 col-lg-1 col-md-1 col-sm-11">
-                            <button type="button" class="btn btn-outline-primary {{ $loop->first ? 'btn-add' : 'btn-remove' }}"><i class="fas fa-{{ $loop->first ? 'plus' : 'minus' }}-square"></i></button>
-                          </div>
-                        </div>
-                      </div>
-                      @if(!$loop->first)
-                      </div>
-                      @endif
-                      @endforeach
+                    <div class="card-body">
+                        <form action="{{ route('timeline.update', $timeline) }}" method="post" enctype="multipart/form-data">
+                            @csrf
+                            @method('PUT')
+                            <div class="form-group">
+                                <label for="title" class="col-form-label">Judul</label> <span
+                                    class="text-danger">*</span>
+                                <input type="text" class="form-control @error('title') is-invalid @enderror"
+                                    name="title" id="title" value="{{ old('title', $timeline->title) }}" required>
+                                @error('title')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="title" class="col-form-label">Judul</label> <span
+                                    class="text-danger">*</span>
+                                <select class="form-control @error('jenis') is-invalid @enderror" name="jenis"
+                                    id="jenis" required>
+                                    <option value="">Pilih Jenis</option>
+                                    <option value="mbkm" {{ old('jenis', $timeline->jenis) == 'mbkm' ? 'selected' : '' }}>
+                                        MBKM</option>
+                                    <option value="instruktur"
+                                        {{ old('jenis', $timeline->jenis) == 'instruktur' ? 'selected' : '' }}>Instruktur
+                                    </option>
+                                    <option value="freelance"
+                                        {{ old('jenis', $timeline->jenis) == 'freelance' ? 'selected' : '' }}>Freelance
+                                    </option>
+                                </select>
+                                @error('jenis')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="deskripsi" class="col-form-label">Deskripsi</label> <span
+                                    class="text-danger">*</span>
+                                <textarea class="summernote-simple @error('deskripsi') is-invalid @enderror" name="deskripsi" id="deskripsi" required>{{ old('tahun_ajaran', $timeline->tahun_ajaran) }}</textarea>
+                                @error('deskripsi')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="status" class="col-form-label">Status</label> <span
+                                    class="text-danger">*</span>
+                                <select name="status" id="status"
+                                    class="form-control @error('status') is-invalid @enderror" required>
+                                    <option value="">Pilih Status</option>
+                                    <option value="1" {{ old('status', $timeline->status) == 1 ? 'selected' : '' }}>
+                                        Aktif</option>
+                                    <option value="0" {{ old('status', $timeline->status) == 0 ? 'selected' : '' }}>
+                                        Tidak Aktif</option>
+                                </select>
+                                @error('status')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="syllabus_link" class="col-form-label">Silabus</label> <span
+                                    class="text-danger">* (Format PDF)
+                                    @if ($timeline->syllabus_link)
+                                        <a href="{{ asset('storage/' . $timeline->syllabus_link) }}" target="_blank">Lihat
+                                            Silabus</a>
+                                    @endif
+                                    <input type="file" class="form-control @error('syllabus_link') is-invalid @enderror"
+                                        name="syllabus_link" id="syllabus_link" value="{{ old('syllabus_link') }}">
+                                    @error('syllabus_link')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
+                            </div>
+                            <div class="row form-group">
+                                <label for="description"
+                                    class="col-xxl-3 col-xl-3 col-lg-3 col-md-3 col-form-label text-left pl-3">Deskripsi
+                                    <span class="text-danger">*</span></label>
+                                <label for="start_at"
+                                    class="col-xxl-3 col-xl-3 col-lg-3 col-md-3 col-form-label text-left pl-3">Tanggal Buka
+                                    <span class="text-danger">*</span></label>
+                                <label for="end_at"
+                                    class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-form-label text-left pl-6">Tanggal Tutup
+                                    <span class="text-danger">*</span></label>
+                            </div>
+                            <div class="row form-group input-copy-here">
+                                @foreach (json_decode($timeline->timeline) as $item)
+                                    @if (!$loop->first)
+                                        <div class="input-group">
+                                    @endif
+                                    <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-3">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" name="description[]" id="description"
+                                                value="{{ $item->title }}" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-3">
+                                        <input type="date" class="form-control" name="start_at[]" id="start_at"
+                                            value="{{ $item->start }}" required>
+                                    </div>
+                                    <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6">
+                                        <div class="row">
+                                            <div class="col-xxl-11 col-xl-11 col-lg-11 col-md-11 col-sm-11">
+                                                <input type="date" class="form-control" name="end_at[]" id="end_at"
+                                                    value="{{ $item->end }}" required>
+                                            </div>
+                                            <div class="col-xxl-1 col-xl-1 col-lg-1 col-md-1 col-sm-11">
+                                                <button type="button"
+                                                    class="btn btn-outline-primary {{ $loop->first ? 'btn-add' : 'btn-remove' }}"><i
+                                                        class="fas fa-{{ $loop->first ? 'plus' : 'minus' }}-square"></i></button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    @if (!$loop->first)
+                            </div>
+                            @endif
+                            @endforeach
                     </div>
                     <button type="submit" class="btn btn-primary">Simpan</button>
-                    <a href="{{route('timeline.index')}}" class="btn btn-danger">Batal</a>
-                  </form>
-                  <div class="row form-group input-copy" style="display: none;">
-                    <div class="input-group">
-                      <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-3">
-                        <div class="form-group">
-                          <input type="text" class="form-control" name="description[]" id="description" required>
+                    <a href="{{ route('timeline.index') }}" class="btn btn-danger">Batal</a>
+                    </form>
+                    <div class="row form-group input-copy" style="display: none;">
+                        <div class="input-group">
+                            <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-3">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" name="description[]" id="description"
+                                        required>
+                                </div>
+                            </div>
+                            <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-3">
+                                <input type="date" class="form-control" name="start_at[]" id="start_at" required>
+                            </div>
+                            <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6">
+                                <div class="row">
+                                    <div class="col-xxl-11 col-xl-11 col-lg-11 col-md-11 col-sm-11">
+                                        <input type="date" class="form-control" name="end_at[]" id="end_at"
+                                            required>
+                                    </div>
+                                    <div class="col-xxl-1 col-xl-1 col-lg-1 col-md-1 col-sm-11">
+                                        <button type="button" class="btn btn-outline-primary btn-remove"><i
+                                                class="fas fa-minus-square"></i></button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                      </div>
-                      <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-3">
-                        <input type="date" class="form-control" name="start_at[]" id="start_at" required>
-                      </div>
-                      <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6">
-                        <div class="row">
-                          <div class="col-xxl-11 col-xl-11 col-lg-11 col-md-11 col-sm-11">
-                            <input type="date" class="form-control" name="end_at[]" id="end_at" required>
-                          </div>
-                          <div class="col-xxl-1 col-xl-1 col-lg-1 col-md-1 col-sm-11">
-                            <button type="button" class="btn btn-outline-primary btn-remove"><i class="fas fa-minus-square"></i></button>
-                          </div>
-                        </div>
-                      </div>
                     </div>
-                  </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+    </div>
 @endsection
 
 @push('scripts')
     <script>
-        $(document).ready(function () {
+        $(document).ready(function() {
 
 
 
-          $('.btn-add').on('click', function () {
-              var input = $('.input-copy').html();
-              $('.input-copy-here').append(input);
-          });
+            $('.btn-add').on('click', function() {
+                var input = $('.input-copy').html();
+                $('.input-copy-here').append(input);
+            });
 
-          $("body").on('click', '.btn-remove', function () {
-              $(this).parents('.input-group').remove();
-          });
+            $("body").on('click', '.btn-remove', function() {
+                $(this).parents('.input-group').remove();
+            });
         });
     </script>
 @endpush
--- resources/views/welcome/instruktur/instruktur-timeline.blade.php
+++ resources/views/welcome/instruktur/instruktur-timeline.blade.php
@@ -1,56 +1,63 @@
 @extends('layouts.welcome.app')
 @section('content')
 
-<div class="page-title">
-  <div class="container">
-    <nav class="breadcrumbs">
-      <ol>
-        <li><a href="{{route('landing.page')}}">Home</a></li>
-        <li class="current">Timeline</li>
-      </ol>
-    </nav>
-    <h1>Timeline</h1>
-  </div>
-</div>
-
-<section id="layanan" class="services section">
-
-  @if (!$timeline)
-  <div class="container section-title" data-aos="fade-up">
-    <h2>Timeline Pendaftaran Instruktur</h2>
-    <p>Belum ada timeline pendaftaran Instruktur</p>
-  </div>
-  @else
-  <!-- Section Title -->
-  <div class="container section-title" data-aos="fade-up">
-    <h2>{{$timeline->title}}</h2>
-    <p>{!! $timeline->tahun_ajaran !!}</p>
-  </div><!-- End Section Title -->
-
-  <div class="container">
-
-    <div class="row gy-4">
-
-      @foreach (json_decode($timeline->timeline) as $item)
-      <div class="col-md-6" data-aos="fade-up" data-aos-delay="100">
-        <div class="service-item d-flex position-relative h-100">
-          <i class="bi-calendar-range icon flex-shrink-0"></i>
-          <div>
-            <h4 class="title">{{$loop->iteration . '. '.$item->title}}</h4>
-            <h5>{{Carbon\Carbon::parse($item->start)->format('d F Y')}} - {{Carbon\Carbon::parse($item->end)->format('d F Y')}}</h5>
-          </div>
+    <div class="page-title">
+        <div class="container">
+            <nav class="breadcrumbs">
+                <ol>
+                    <li><a href="{{ route('landing.page') }}">Home</a></li>
+                    <li class="current">Timeline</li>
+                </ol>
+            </nav>
+            <h1>Timeline</h1>
         </div>
-      </div><!-- End Service Item -->
-      @endforeach
-
     </div>
-    {{-- button to register --}}
-    <div class="container section-title mt-4" data-aos="fade-up" data-aos-delay="100">
-      <a href="{{route('register.instruktur')}}" class="btn-daftar-layanan">Daftar</a>
-    </div><!-- End Section Title -->
-  </div>
-  @endif
 
-</section>
+    <section id="layanan" class="services section">
+
+        @if (!$timeline)
+            <div class="container section-title" data-aos="fade-up">
+                <h2>Timeline Pendaftaran Instruktur</h2>
+                <p>Belum ada timeline pendaftaran Instruktur</p>
+            </div>
+        @else
+            <!-- Section Title -->
+            <div class="container section-title" data-aos="fade-up">
+                <h2>{{ $timeline->title }}</h2>
+                <p>{!! $timeline->tahun_ajaran !!}</p>
+                <br>
+                @if ($timeline->syllabus_link)
+                    <h2>Silabus</h2>
+                    <embed src="{{ asset('storage/' . $timeline->syllabus_link) }}" type="application/pdf" width="100%"
+                        height="600px">
+                @endif
+            </div><!-- End Section Title -->
+
+            <div class="container">
+
+                <div class="row gy-4">
+
+                    @foreach (json_decode($timeline->timeline) as $item)
+                        <div class="col-md-6" data-aos="fade-up" data-aos-delay="100">
+                            <div class="service-item d-flex position-relative h-100">
+                                <i class="bi-calendar-range icon flex-shrink-0"></i>
+                                <div>
+                                    <h4 class="title">{{ $loop->iteration . '. ' . $item->title }}</h4>
+                                    <h5>{{ Carbon\Carbon::parse($item->start)->format('d F Y') }} -
+                                        {{ Carbon\Carbon::parse($item->end)->format('d F Y') }}</h5>
+                                </div>
+                            </div>
+                        </div><!-- End Service Item -->
+                    @endforeach
+
+                </div>
+                {{-- button to register --}}
+                <div class="container section-title mt-4" data-aos="fade-up" data-aos-delay="100">
+                    <a href="{{ route('register.instruktur') }}" class="btn-daftar-layanan">Daftar</a>
+                </div><!-- End Section Title -->
+            </div>
+        @endif
+
+    </section>
 
 @endsection
